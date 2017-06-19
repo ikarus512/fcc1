920b14274f71e53cb92ec05215c4f08623311b32
ikarus512 <ikarus512@yandex.ru>
2017-06-19 15:18:34 +0300

    app2: adding leaflet offline map

--- src/js/common/app2/directive-map-leaflet.js
+++ src/js/common/app2/directive-map-leaflet.js
@@ -9,7 +9,24 @@
 
   angular.module('myapp')
 
-  .directive('myLeafletMap', ['$timeout', 'MyConst', function($timeout, MyConst) {
+  .directive('myLeafletMap', ['MyConst', function(MyConst) {
+
+    var layerOffline = L.tileLayer(
+      // 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      MyConst.urlPref + 'img/OSM5/{z}/{x}/{y}.png', {
+        reuseTiles: true,
+        updateWhenIdle: false,
+        maxZoom: 17,
+    }); //.addTo(map);
+    var layerOnline = L.tileLayer(
+      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      // 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      // MyConst.urlPref + 'img/OSM5/{z}/{x}/{y}.png', {
+        reuseTiles: true,
+        updateWhenIdle: false,
+        maxZoom: 17,
+    }); //.addTo(map);
+
 
     var scope = {
       cafes: '=mapCafes',
@@ -28,19 +45,81 @@
 
       var map = null;
 
+      var mapOptions = {
+        center: scope.center, // L.LatLng(scope.center.lat, scope.center.lng),
+        zoom: scope.zoom,
+        // mapTypeId: google.maps.MapTypeId.ROADMAP,
+        // fullscreenControl: true,
+        // mapTypeControl: true,
+        // scaleControl: true,
+          // trackResize: true,
+        zoomControl: true,
+        doubleClickZoom: false, //disableDoubleClickZoom: true, // we do it manually
+        layers: [layerOffline],
+      };
+
       initMap();
 
       function initMap() {
+        var
+          mainElem = angular.element(element[0]),
+          mapElem = angular.element(element.find('#leaflet-map-canvas')[0]);
 
-        scope.goOnline = function() {
-          console.log('goOnline()');
+        scope.nextLayerName = 'go online';
+        scope.toggleLayer = function() {
+          if (map && map.hasLayer(layerOffline)) {
+            scope.nextLayerName = 'go offline';
+            map.removeLayer(layerOffline);
+            map.addLayer(layerOnline);
+          } else {
+            scope.nextLayerName = 'go online';
+            map.removeLayer(layerOnline);
+            map.addLayer(layerOffline);
+          }
+        }; // scope.toggleLayer = function(...)
+
+        var
+          fullscreen = false,
+          curOpts = {},
+          el = mainElem;
+        curOpts = {
+          position: el.css('position'),
+          left:     el.css('left'),
+          top:      el.css('top'),
+          height:   el.css('height'),
+          width:    '100%', //el.css('width'),
+          'z-index':el.css('z-index'),
         };
+        scope.fullscreenIcon = 'zoom_out_map';
+        scope.toggleFullscreen = function() {
+          if (fullscreen) {
+            // Here if current map mode is fullscreen.
+            // Going to small screen.
+            scope.fullscreenIcon = 'zoom_out_map';
+            el.css(curOpts);
+            angular.element('body').css({overflow:'auto'})
+          } else {
+            // Here if current map mode: small screen.
+            // Going to fullscreen.
+            scope.fullscreenIcon = 'fullscreen_exit';
+            el.css({
+              position:'fixed',
+              left:  0,
+              top:   0,
+              height:'100%',
+              width: '100%',
+              'z-index':1500,
+            });
+            angular.element('body').css({overflow:'hidden'})
+          }
+          fullscreen = !fullscreen;
+        }; // scope.toggleFullscreen = function(...)
 
         if (!map) {
 
-          var map = L.map(element.find('#leaflet-map-canvas')[0])
+          map = L.map(element.find('#leaflet-map-canvas')[0], mapOptions);
 
-          .setView([56.312956, 43.989955], 17);
+          // .setView([56.312956, 43.989955], 17);
 
           L.tileLayer(
             // 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -59,10 +138,10 @@
    
     return {
       restrict: 'E',
-      template: '<div><button ng-click="goOnline();">Go Online</button><div id="leaflet-map-canvas" style="height:90%;width:100%;"></div></div>',
+      templateUrl: MyConst.urlPref + 'js/common/app2/directive-map-leaflet.html',
       replace: true,
       scope: scope,
-      link: directiveLinkFunction
+      link: directiveLinkFunction,
     };
   }]);
 
