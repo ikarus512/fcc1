61ce68c8f649816595385ea7eabad03e75cee20f
ikarus512 <ikarus512@yandex.ru>
2017-07-23 13:13:00 +0300

    cosmetic

--- .update.node.js.bat
+++ .update.node.js.bat
@@ -1,1 +1,4 @@
-https://nodejs.org/download/release/latest-v6.x/
+rem https://nodejs.org
+wget https://nodejs.org/download/release/latest-v6.x/win-x64/node.exe -O $(which node).exe
+rem https://docs.npmjs.com
+npm i -g npm
