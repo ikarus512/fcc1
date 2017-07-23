37606be996625e14d36ba9dfcc5a15d5b4531b25
ikarus512 <ikarus512@yandex.ru>
2017-07-23 13:40:45 +0300

    cosmetic

--- .update.node.js.bat
+++ .update.node.js.bat
@@ -1,4 +1,12 @@
 rem https://nodejs.org
 wget https://nodejs.org/download/release/latest-v6.x/win-x64/node.exe -O $(which node).exe
+
 rem https://docs.npmjs.com
 npm i -g npm
+
+npm outdated -g --depth=0
+npm cache clean
+npm update -g
+
+npm outdated
+npm update
