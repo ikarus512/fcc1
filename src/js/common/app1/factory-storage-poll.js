ebbfcbc3e87d9a3eabfb2838320f470f2002c435
ikarus512 <ikarus512@yandex.ru>
2017-08-08 12:19:21 +0300

    code style fix

--- src/js/common/app1/factory-storage-poll.js
+++ src/js/common/app1/factory-storage-poll.js
@@ -7,50 +7,60 @@
 (function() {
     'use strict';
 
-    angular.module('myapp')
+    angular
+    .module('myapp')
+    .factory('StoragePoll', StoragePoll);
 
-    .factory('StoragePoll', ['$http', 'MyConst', function StoragePoll($http, MyConst) {
+    StoragePoll.$inject = ['$http', 'MyConst'];
+
+    function StoragePoll($http, MyConst) {
         return {
-            get: function getPoll(pollId) {
-                return $http({
-                    method: 'GET',
-                    data: {},
-                    url: MyConst.serverUrl + '/app1/api/polls/' + pollId
-                });
-            },
-
-            delete: function deletePoll(pollId) { // delete poll
-                return $http({
-                    method: 'DELETE',
-                    data: {},
-                    url: MyConst.serverUrl + '/app1/api/polls/' + pollId
-                });
-            },
-
-            post: function postPoll(pollId, title) { // create new option
-                return $http({
-                    method: 'POST',
-                    data: {title: title},
-                    url: MyConst.serverUrl + '/app1/api/polls/' + pollId + '/options'
-                });
-            },
-
-            put: function putPoll(pollId, optId) { // vote for poll option
-                return $http({
-                    method: 'PUT',
-                    data: {},
-                    url:
-                        MyConst.serverUrl +
-                        '/app1/api/polls/' +
-                        pollId +
-                        '/options/' +
-                        optId +
-                        '/vote'
-                });
-            }
-
+            get: getPoll,
+            delete: deletePoll,
+            post: postPoll,
+            put: putPoll
         };
 
-    }]);
+        ////////////////////////////////////////
+
+        function getPoll(pollId) {
+            return $http({
+                method: 'GET',
+                data: {},
+                url: MyConst.serverUrl + '/app1/api/polls/' + pollId
+            });
+        } // function getPoll(...)
+
+        function deletePoll(pollId) { // delete poll
+            return $http({
+                method: 'DELETE',
+                data: {},
+                url: MyConst.serverUrl + '/app1/api/polls/' + pollId
+            });
+        } // function deletePoll(...)
+
+        function postPoll(pollId, title) { // create new option
+            return $http({
+                method: 'POST',
+                data: {title: title},
+                url: MyConst.serverUrl + '/app1/api/polls/' + pollId + '/options'
+            });
+        } // function postPoll(...)
+
+        function putPoll(pollId, optId) { // vote for poll option
+            return $http({
+                method: 'PUT',
+                data: {},
+                url:
+                    MyConst.serverUrl +
+                    '/app1/api/polls/' +
+                    pollId +
+                    '/options/' +
+                    optId +
+                    '/vote'
+            });
+        } // function putPoll(...)
+
+    } // function StoragePoll(...)
 
 }());
