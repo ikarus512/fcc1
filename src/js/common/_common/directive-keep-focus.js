9dbc596721b7c489b9576f638929e2101522ff82
ikarus512 <ikarus512@yandex.ru>
2017-08-10 14:23:56 +0300

    clients directive-keep-focus.js documented

--- src/js/common/_common/directive-keep-focus.js
+++ src/js/common/_common/directive-keep-focus.js
@@ -4,28 +4,81 @@
  * https://github.com/ikarus512/fcc1.git
  */
 
+/**
+ * @namespace Directives
+ * @desc common directives
+ * @memberOf clients.Modules._common
+ */
 (function() {
     'use strict';
 
-    angular.module('_common')
+    angular
+    .module('_common')
+    .directive('keepFocus', keepFocus);
 
-    .directive('keepFocus', ['$timeout', function keepFocus($timeout) {
-        return {
+    keepFocus.$inject = [
+        '$timeout'
+    ];
+
+    /**
+     * @classdesc keepFocus directive (keep focus after ng-repeat sorting)
+     * @class
+     * @param {Object} $timeout
+     * @memberOf clients.Modules._common.Directives
+     *
+     * @example
+     *
+     * <textarea
+     *     keep-focus>
+     *     keep-focus-var='{{bid.biddersEditMsgFocusVar}}'
+     *     keep-focus-index='{{$index}}'
+     * >
+     * </textarea>
+     */
+    function keepFocus($timeout) {
+
+        /**
+         * @member {Object}
+         * @memberOf clients.Modules._common.Directives.keepFocus
+         * @type {Object}
+         *
+         * @property {String} restrict Restricted to attributes
+         * @property {Object} scope Scoped variables
+         * @property {Object} scope.keepFocusVar  keepFocus variable name
+         * @property {number} scope.keepFocusIndex keepFocus index
+         * @property {function} link Link fuction
+         */
+        var directive = {
             restrict: 'A',
             scope: {
                 keepFocusVar: '@',
                 keepFocusIndex: '@'
             },
-            link: function linkFunction(scope, element, attrs) {
-                scope.$watch('keepFocusIndex', function(newVal, prevVal) {
-                    if (scope.keepFocusVar) {
-                        $timeout(function() {
-                            element[0].focus();
-                        });
-                    }
-                });
-            } // link: function linkFunction(...)
+            link: linkFunction
         };
-    }]); // .directive('keepFocus', ...
+
+        return directive;
+
+        ////////////////////////////////////////
+
+        /**
+         * Link function
+         * @static
+         * @param {Object} scope
+         * @param {Object} element
+         * @param {Object} attrs
+         * @memberOf clients.Modules._common.Directives.keepFocus
+         */
+        function linkFunction(scope, element, attrs) {
+            scope.$watch('keepFocusIndex', function(newVal, prevVal) {
+                if (scope.keepFocusVar) {
+                    $timeout(function() {
+                        element[0].focus();
+                    });
+                }
+            });
+        } // function linkFunction(...)
+
+    } // function keepFocus(...)
 
 }());
